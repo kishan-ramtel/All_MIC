c5b98578c94b077101bbb3e2505f159113226984
kishan <kishanramtel0@gmail.com>
2025-05-27 18:13:48 +0545

    fet: starting setup!

---
+++ backend/src/main/java/com/latest/allmic/AllMicApplication.java
@@ -0,0 +1,13 @@
+package com.latest.allmic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllMicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllMicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/latest/allmic/AllMicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latest.allmic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllMicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
